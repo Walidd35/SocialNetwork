e2d7d88223d2f63289268c908b39b8199d6c5eb9
walid <walidbahij35@gmail.com>
2024-11-27 20:57:29 +0100

    MAJ

--- back/configbdd/db.js
+++ back/configbdd/db.js
@@ -2,7 +2,7 @@ const { Sequelize } = require('sequelize');
 require('dotenv').config({ path: '../.env' });
 
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-  host: 'localhost', //mettre "mysql" pour tourner docker
+  host: 'mysql', //mettre "mysql" pour tourner docker
   dialect: 'mysql',
   port: 3306,
 });
